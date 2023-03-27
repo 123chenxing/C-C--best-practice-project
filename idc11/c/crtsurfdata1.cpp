f7711723658787826b474e14902e43ae64073691
1980297599@qq.com <1980297599@qq.com>
2023-03-27 17:10:41 +0800

    first commit

---
+++ idc11/c/crtsurfdata1.cpp
@@ -0,0 +1,20 @@
+#include "_public.h"
+CLogFile logfile;
+int main(int argc,char *argv[]){
+//inifile outpath logfile
+if(argc!=4){
+printf("Using:./crtsurfdata1 inifile outpath logfile\n");
+printf("Example:/project/idc11/bin/crtsurfdata1 /project/idc11/ini/stcode.ini /tmp/surfdata /log/idc/crtsurfdata1.log\n\n");
+printf("inifile 全国气象站点参数文件名。\n");
+printf("outpath 全国气象站点数据文件存放的目录。\n");
+printf("logfile 本程序运行的日志文件名。\n\n");
+return -1;  
+}
+if(logfile.Open(argv[3],"a+","false")==false){
+printf("logfile.Open(%s) failed.\n",argv[3]); return -1;
+}
+logfile.Write("crtsurfdata1 开始运行。\n");
+//这里插入业务代码
+logfile.WriteEx("crtsurfdata1 运行结束。\n");
+return 0;
+}
